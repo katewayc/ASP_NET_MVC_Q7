550078d31add307c9cf6f6be1e2ef919622f76c5
張齊允-Katec <katec@bqool.com.tw>
2019-07-15 14:30:03 +0800

    remove unused jQuery validation and add vue.js

--- source/TodoMVC.Web/App_Start/BundleConfig.cs
+++ source/TodoMVC.Web/App_Start/BundleConfig.cs
@@ -10,14 +10,14 @@ namespace TodoMVC
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/vue").Include(
+          "~/Scripts/vue.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
